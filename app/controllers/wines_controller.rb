4926fdb527a6b0ae14bcb78def8ddb2d89123440
Laurence Mouren <laurence.mouren@gmail.com>
2020-09-19 14:14:03 +0200

    deb form

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,4 +1,20 @@
 class WinesController < ApplicationController
 
+  def new
+    @wine = Wine.new
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    @wine.save
+
+    redirect_to root_path
+  end
+
+  private
+
+  def wine_params
+    params_require(:wine).permit(:name, :description, :user_id, :price_id, :region_id, :country)
+  end
 
 end
